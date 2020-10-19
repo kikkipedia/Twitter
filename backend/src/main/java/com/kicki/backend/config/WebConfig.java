76850cae7d7e635e27ae8cf8191ebf1def776487
kikkipedia <kristinakerlund@gmail.com>
2020-10-19 16:01:23 +0200

    commit

--- backend/src/main/java/com/kicki/backend/config/WebConfig.java
+++ backend/src/main/java/com/kicki/backend/config/WebConfig.java
@@ -5,11 +5,9 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class AppConfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**")
-		.allowedOrigins("http://localhost:3000")
-		.allowedMethods("POST", "GET");
+		registry.addMapping("/**").allowedMethods("*");
 	}
 }
